78bf52c38ed9b0d6f739f0a6b677a79581ebb1a8
soluciaAcademics <solucia513@gmail.com>
2024-05-15 10:19:32 +0400

    Solucia file upload

---
+++ scripts/onlineresources.js
@@ -0,0 +1,40 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+function canva()
+{
+    window.open("https://www.canva.com");
+    window.location.replace("onlineresources.html");
+}
+
+function figma()
+{
+    window.open("https://www.figma.com");
+    window.location.replace("onlineresources.html");
+}
+
+function vscode()
+{
+    window.open("https://code.visualstudio.com");
+    window.location.replace("onlineresources.html");
+}
+
+function sway()
+{
+    window.open("https://sway.cloud.microsoft");
+    window.location.replace("onlineresources.html");
+}
+
+function githubdesktop()
+{
+    window.open("https://desktop.github.com");
+    window.location.replace("onlineresources.html");
+}
